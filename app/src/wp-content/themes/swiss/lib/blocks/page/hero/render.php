034ff0a2e40832a22d944cf49bde3a94e516590b
Oliver Granlund <oliver.granlund@evermade.fi>
2019-08-16 12:27:53 +0000

    Hero fixes

--- app/src/wp-content/themes/swiss/lib/blocks/page/hero/render.php
+++ app/src/wp-content/themes/swiss/lib/blocks/page/hero/render.php
@@ -3,4 +3,4 @@
 // create a new block object to work with
 $block = new \Evermade\Swiss\Block(['background_image', 'text', 'video_title','video_link_text','video_link_url','video_thumbnail','opening_hours', 'countdown']);
 
-if(!empty($block->get('text'))) include (__DIR__.'/templates/view.php');
+include (__DIR__.'/templates/view.php');
